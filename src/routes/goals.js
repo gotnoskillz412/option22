b32453f997d07a2af386581003714a1888c9800f
Spencer Hockeborn <spencer.hockeborn@dudesolutions.com>
2017-08-23 19:39:44 -0400

    Adding unit tests for goal and subgoal mongohelpers

--- src/routes/goals.js
+++ src/routes/goals.js
@@ -9,14 +9,14 @@ const router = new express.Router();
 
 router.get('/', (req, res) => {
     let profileGoals;
-    mongoHelpers.findProfile({accountId: req.account._id})
+    mongoHelpers.findProfile({ accountId: req.account._id })
         .then((profile) => {
-            return mongoHelpers.findAllGoals({profileId: profile._id})
+            return mongoHelpers.findAllGoals({ profileId: profile._id })
         })
         .then((goals) => {
             profileGoals = JSON.parse(JSON.stringify(goals));
             let promises = goals.map((goal) => {
-                return mongoHelpers.findAllSubgoals({goalId: goal._id});
+                return mongoHelpers.findAllSubgoals({ goalId: goal._id });
             });
             return Promise.all(promises);
         })
@@ -24,7 +24,7 @@ router.get('/', (req, res) => {
             profileGoals.forEach((goal, index) => {
                 goal['subgoals'] = subgoals[index];
             });
-            res.status(200).json({total: profileGoals.length, data: profileGoals});
+            res.status(200).json({ total: profileGoals.length, data: profileGoals });
         })
         .catch((err) => {
             logger.error('goals', 'Error getting goals', err);
@@ -40,7 +40,7 @@ router.post('/', (req, res) => {
 
     return mongoHelpers.addGoal(req.body)
         .then((goal) => {
-        profileGoal = goal;
+            profileGoal = goal;
             let promises = subgoals.map((subgoal) => {
                 subgoal.goalId = goal._id;
                 return mongoHelpers.addSubgoal(subgoal);
@@ -48,7 +48,7 @@ router.post('/', (req, res) => {
             return Promise.all(promises);
         })
         .then((subgoals) => {
-            profileGoal.subgoals = subgoals;
+            profileGoal.subgoals = JSON.parse(JSON.stringify(subgoals)) || [];
             res.status(201).json(profileGoal);
         })
         .catch((err) => {
@@ -59,29 +59,136 @@ router.post('/', (req, res) => {
 
 // remove goal
 router.delete('/:goalId', (req, res) => {
-    return mongoHelpers.findGoal({_id: req.params.goalId})
+    return mongoHelpers.findGoal({ _id: req.params.goalId })
         .then((goal) => {
-            return mongoHelpers.findAllSubgoals({goalId: goal._id});
+            if (!goal) {
+                res.status(404).send();
+                return Promise.reject(404);
+            }
+            return mongoHelpers.findAllSubgoals({ goalId: goal._id });
         })
         .then((subgoals) => {
             let promises = subgoals.map((subgoal) => {
-                return mongoHelpers.removeSubgoal({_id: subgoal._id});
+                return mongoHelpers.removeSubgoal({ _id: subgoal._id });
             });
             return Promise.all(promises);
         })
         .then(() => {
-            return mongoHelpers.removeGoal({_id: req.params.goalId})
+            return mongoHelpers.removeGoal({ _id: req.params.goalId })
         })
         .then(() => {
             res.status(202).send('Deleted');
         }).catch((err) => {
-            logger.error('goals', 'Error deleting goal', err);
-            res.status(500).json({message: 'Internal server error deleting goal'});
+            if (err !== 404) {
+                logger.error('goals', 'Error deleting goal', err);
+                res.status(500).json({ message: 'Internal server error deleting goal' });
+            }
         });
 });
 
 // get single goal
+router.get('/:goalId', (req, res) => {
+    let profileGoal;
+    return mongoHelpers.findGoal({ _id: req.params.goalId })
+        .then((goal) => {
+            if (!goal) {
+                res.status(404).send();
+                return Promise.reject(404);
+            }
+            profileGoal = JSON.parse(JSON.stringify(goal));
+            return mongoHelpers.findAllSubgoals({ goalId: goal._id });
+        })
+        .then((subgoals) => {
+            profileGoal.subgoals = JSON.parse(JSON.stringify(subgoals)) || [];
+            res.status(200).json(profileGoal);
+        })
+        .catch((err) => {
+            if (err !== 404) {
+                logger.error('goals', 'Error getting goal', err);
+                res.status(500).json({ message: 'Internal server error getting goal' });
+            }
+        });
+});
 
 // update goal
+router.put('/:goalId', (req, res) => {
+    let updatedGoal = req.body;
+    let updatedSubgoals = updatedGoal.subgoals && JSON.parse(JSON.stringify(updatedGoal.subgoals));
+    let subgoalsToRemove;
+    delete updatedGoal.subgoals;
+    let goalPromises = [];
+    let savedGoal;
+    let savedSubgoals;
+
+    goalPromises.push(new Promise((resolve, reject) => {
+        mongoHelpers.findGoal({ _id: req.params.goalId })
+            .then((goal) => {
+                Object.keys(updatedGoal).forEach((key) => {
+                    if (goal[key] !== updatedGoal[key]) {
+                        goal[key] = updatedGoal[key];
+                    }
+                });
+                goal.save((err, result) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        savedGoal = JSON.parse(JSON.stringify(result));
+                        resolve();
+                    }
+                });
+            });
+    }));
+    goalPromises.push(new Promise((resolve, reject) => {
+        mongoHelpers.findAllSubgoals({ goalId: updatedGoal._id })
+            .then((subgoals) => {
+                subgoalsToRemove = subgoals.filter((s) => {
+                    let found = updatedSubgoals.find((us) => {
+                        return s._id === us._id;
+                    });
+                    return !found;
+                });
+            })
+            .then(() => {
+                let subgoalPromises = updatedSubgoals.map((updatedSubgoal) => {
+                    return new Promise((resolve2, reject2) => {
+                        mongoHelpers.findSubgoal({_id: updatedSubgoal._id})
+                            .then((sg) => {
+                                Object.keys(updatedGoal).forEach((key) => {
+                                    if (sg[key] !== updatedSubgoal[key]) {
+                                        sg[key] = updatedSubgoal[key];
+                                    }
+                                });
+                                sg.save((err, result) => {
+                                    if (err) {
+                                        reject2(err);
+                                    } else {
+                                        resolve2(result);
+                                    }
+                                });
+                            });
+                    });
+                });
+                return Promise.all(subgoalPromises);
+            })
+            .then((subgoals) => {
+                savedSubgoals = JSON.parse(JSON.stringify(subgoals));
+                return new Promise((resolve3, reject3) => {
+                    subgoalsToRemove = subgoalsToRemove.map((sgtr) => {
+                        return mongoHelpers.removeSubgoal({_id: sgtr._id});
+                    });
+                    Promise.all(subgoalsToRemove)
+                        .then(resolve3, reject3);
+                });
+            })
+            .then(resolve, reject);
+    }));
+
+    Promise.all(goalPromises).then(() => {
+        savedGoal.subgoals = savedSubgoals || [];
+    }).catch((err) => {
+        logger.error('goals', 'Error updating goal and subgoals', err);
+        res.status(500).json({message: 'Internal server error updating goal and subgoals'});
+    });
+});
 
 exports = module.exports = router;
--- test/helpers/mongoHelpers.spec.js
+++ test/helpers/mongoHelpers.spec.js
@@ -3,7 +3,9 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 
 const Account = require('../../src/models/account.mongoose');
+const Goal = require('../../src/models/goal.mongoose');
 const Profile = require('../../src/models/profile.mongoose');
+const Subgoal = require('../../src/models/subgoal.mongoose');
 
 const constants = require('../../src/helpers/constants.json');
 const mongoHelpers = require('../../src/helpers/mongoHelpers');
@@ -158,7 +160,7 @@ describe('Mongo Helpers', function () {
         });
         mongoHelpers.findAllProfiles({}).then(done, (result) => {
             expect(findStub.calledOnce).to.eql(true, 'Expected "findStub" to be called once');
-            expect(result.step).to.eql(constants.mongo.steps.profileFind, `Expected '${result.step}' to be '${constants.mongo.steps.profileFind}'`);
+            expect(result.step).to.eql(constants.mongo.steps.profileFindAll, `Expected '${result.step}' to be '${constants.mongo.steps.profileFindAll}'`);
             expect(result.message).to.eql('Error finding profiles', `Expected '${result.message}' to be 'Error finding profiles'`);
             expect(result.error).to.eql('err', `Expected '${result.message}' to be 'err'`);
             done();
@@ -184,6 +186,202 @@ describe('Mongo Helpers', function () {
             expect(removeStub.calledOnce).to.eql(true, 'Expected "removeStub" to be called once');
             expect(result.step).to.eql(constants.mongo.steps.profileDelete, `Expected '${result.step}' to be '${constants.mongo.steps.profileDelete}'`);
             expect(result.message).to.eql('Error deleting profile', `Expected '${result.message}' to be 'Error deleting profile'`);
+            expect(result.error).to.eql('err', `Expected '${result.message}' to be 'err'`);
+            done();
+        });
+    });
+
+    it('should test the addGoal method (succeed)', (done) => {
+        const saveStub = this.sandbox.stub(Goal.prototype, 'save').callsFake((cb) => {
+            cb(null, 'success');
+        });
+
+        mongoHelpers.addGoal({}).then((result) => {
+            expect(saveStub.calledOnce).to.eql(true, 'Expected "saveStub" to be called once');
+            expect(result).to.eql('success', `Expected '${result}' to be 'success'`);
+            done();
+        }, done);
+    });
+
+    it('should test the createGoal method (failed)', (done) => {
+        const saveStub = this.sandbox.stub(Goal.prototype, 'save').callsFake((cb) => {
+            cb('err', null);
+        });
+
+        mongoHelpers.addGoal({}).then(done, (result) => {
+            expect(saveStub.calledOnce).to.eql(true, 'Expected "saveStub" to be called once');
+            expect(result.step).to.eql(constants.mongo.steps.goalAdd, `Expected '${result.step}' to be '${constants.mongo.steps.goalAdd}'`);
+            expect(result.message).to.eql('Error adding goal', `Expected '${result.message}' to be 'Error adding goal'`);
+            expect(result.error).to.eql('err', `Expected '${result.message}' to be 'err'`);
+            done();
+        });
+    });
+
+    it('should test the findGoal method (success)', (done) => {
+        const findOneStub = this.sandbox.stub(Goal, 'findOne').callsFake((params, cb) => {
+            cb(null, 'success');
+        });
+        mongoHelpers.findGoal({}).then((result) => {
+            expect(findOneStub.calledOnce).to.eql(true, 'Expected "findOneStub" to be called once');
+            expect(result).to.eql('success', `Expected '${result}' to be 'success'`);
+            done();
+        }, done);
+    });
+
+    it('should test the findGoal method (failed)', (done) => {
+        const findOneStub = this.sandbox.stub(Goal, 'findOne').callsFake((params, cb) => {
+            cb('err', null);
+        });
+        mongoHelpers.findGoal({}).then(done, (result) => {
+            expect(findOneStub.calledOnce).to.eql(true, 'Expected "findOneStub" to be called once');
+            expect(result.step).to.eql(constants.mongo.steps.goalFind, `Expected '${result.step}' to be '${constants.mongo.steps.goalFind}'`);
+            expect(result.message).to.eql('Error finding goal', `Expected '${result.message}' to be 'Error finding goal'`);
+            expect(result.error).to.eql('err', `Expected '${result.message}' to be 'err'`);
+            done();
+        });
+    });
+
+    it('should test the findAllGoals method (success)', (done) => {
+        const findStub = this.sandbox.stub(Goal, 'find').callsFake((params, cb) => {
+            cb(null, 'success');
+        });
+        mongoHelpers.findAllGoals({}).then((result) => {
+            expect(findStub.calledOnce).to.eql(true, 'Expected "findStub" to be called once');
+            expect(result).to.eql('success', `Expected '${result}' to be 'success'`);
+            done();
+        }, done);
+    });
+
+    it('should test the findAllGoals method (failed)', (done) => {
+        const findStub = this.sandbox.stub(Goal, 'find').callsFake((params, cb) => {
+            cb('err', null);
+        });
+        mongoHelpers.findAllGoals({}).then(done, (result) => {
+            expect(findStub.calledOnce).to.eql(true, 'Expected "findStub" to be called once');
+            expect(result.step).to.eql(constants.mongo.steps.goalFindAll, `Expected '${result.step}' to be '${constants.mongo.steps.goalFindAll}'`);
+            expect(result.message).to.eql('Error finding goals', `Expected '${result.message}' to be 'Error finding goal'`);
+            expect(result.error).to.eql('err', `Expected '${result.message}' to be 'err'`);
+            done();
+        });
+    });
+
+    it('should test the removeGoal method (success)', (done) => {
+        const removeStub = this.sandbox.stub(Goal, 'remove').callsFake((params, cb) => {
+            cb(null, 'success');
+        });
+        mongoHelpers.removeGoal({}).then((result) => {
+            expect(removeStub.calledOnce).to.eql(true, 'Expected "removeStub" to be called once');
+            expect(result).to.eql('success', `Expected '${result}' to be 'success'`);
+            done();
+        }, done);
+    });
+
+    it('should test the removeGoal method (failed)', (done) => {
+        const removeStub = this.sandbox.stub(Goal, 'remove').callsFake((params, cb) => {
+            cb('err', null);
+        });
+        mongoHelpers.removeGoal({}).then(done, (result) => {
+            expect(removeStub.calledOnce).to.eql(true, 'Expected "removeStub" to be called once');
+            expect(result.step).to.eql(constants.mongo.steps.goalDelete, `Expected '${result.step}' to be '${constants.mongo.steps.goalDelete}'`);
+            expect(result.message).to.eql('Error deleting goal', `Expected '${result.message}' to be 'Error deleting goal'`);
+            expect(result.error).to.eql('err', `Expected '${result.message}' to be 'err'`);
+            done();
+        });
+    });
+
+    it('should test the addSubgoal method (succeed)', (done) => {
+        const saveStub = this.sandbox.stub(Subgoal.prototype, 'save').callsFake((cb) => {
+            cb(null, 'success');
+        });
+
+        mongoHelpers.addSubgoal({}).then((result) => {
+            expect(saveStub.calledOnce).to.eql(true, 'Expected "saveStub" to be called once');
+            expect(result).to.eql('success', `Expected '${result}' to be 'success'`);
+            done();
+        }, done);
+    });
+
+    it('should test the createSubgoal method (failed)', (done) => {
+        const saveStub = this.sandbox.stub(Subgoal.prototype, 'save').callsFake((cb) => {
+            cb('err', null);
+        });
+
+        mongoHelpers.addSubgoal({}).then(done, (result) => {
+            expect(saveStub.calledOnce).to.eql(true, 'Expected "saveStub" to be called once');
+            expect(result.step).to.eql(constants.mongo.steps.subgoalAdd, `Expected '${result.step}' to be '${constants.mongo.steps.subgoalAdd}'`);
+            expect(result.message).to.eql('Error adding subgoal', `Expected '${result.message}' to be 'Error adding subgoal'`);
+            expect(result.error).to.eql('err', `Expected '${result.message}' to be 'err'`);
+            done();
+        });
+    });
+
+    it('should test the findSubgoal method (success)', (done) => {
+        const findOneStub = this.sandbox.stub(Subgoal, 'findOne').callsFake((params, cb) => {
+            cb(null, 'success');
+        });
+        mongoHelpers.findSubgoal({}).then((result) => {
+            expect(findOneStub.calledOnce).to.eql(true, 'Expected "findOneStub" to be called once');
+            expect(result).to.eql('success', `Expected '${result}' to be 'success'`);
+            done();
+        }, done);
+    });
+
+    it('should test the findSubgoal method (failed)', (done) => {
+        const findOneStub = this.sandbox.stub(Subgoal, 'findOne').callsFake((params, cb) => {
+            cb('err', null);
+        });
+        mongoHelpers.findSubgoal({}).then(done, (result) => {
+            expect(findOneStub.calledOnce).to.eql(true, 'Expected "findOneStub" to be called once');
+            expect(result.step).to.eql(constants.mongo.steps.subgoalFind, `Expected '${result.step}' to be '${constants.mongo.steps.subgoalFind}'`);
+            expect(result.message).to.eql('Error finding subgoal', `Expected '${result.message}' to be 'Error finding subgoal'`);
+            expect(result.error).to.eql('err', `Expected '${result.message}' to be 'err'`);
+            done();
+        });
+    });
+
+    it('should test the findAllSubgoals method (success)', (done) => {
+        const findStub = this.sandbox.stub(Subgoal, 'find').callsFake((params, cb) => {
+            cb(null, 'success');
+        });
+        mongoHelpers.findAllSubgoals({}).then((result) => {
+            expect(findStub.calledOnce).to.eql(true, 'Expected "findStub" to be called once');
+            expect(result).to.eql('success', `Expected '${result}' to be 'success'`);
+            done();
+        }, done);
+    });
+
+    it('should test the findAllSubgoals method (failed)', (done) => {
+        const findStub = this.sandbox.stub(Subgoal, 'find').callsFake((params, cb) => {
+            cb('err', null);
+        });
+        mongoHelpers.findAllSubgoals({}).then(done, (result) => {
+            expect(findStub.calledOnce).to.eql(true, 'Expected "findStub" to be called once');
+            expect(result.step).to.eql(constants.mongo.steps.subgoalFindAll, `Expected '${result.step}' to be '${constants.mongo.steps.subgoalFindAll}'`);
+            expect(result.message).to.eql('Error finding subgoals', `Expected '${result.message}' to be 'Error finding subgoal'`);
+            expect(result.error).to.eql('err', `Expected '${result.message}' to be 'err'`);
+            done();
+        });
+    });
+
+    it('should test the removeSubgoal method (success)', (done) => {
+        const removeStub = this.sandbox.stub(Subgoal, 'remove').callsFake((params, cb) => {
+            cb(null, 'success');
+        });
+        mongoHelpers.removeSubgoal({}).then((result) => {
+            expect(removeStub.calledOnce).to.eql(true, 'Expected "removeStub" to be called once');
+            expect(result).to.eql('success', `Expected '${result}' to be 'success'`);
+            done();
+        }, done);
+    });
+
+    it('should test the removeSubgoal method (failed)', (done) => {
+        const removeStub = this.sandbox.stub(Subgoal, 'remove').callsFake((params, cb) => {
+            cb('err', null);
+        });
+        mongoHelpers.removeSubgoal({}).then(done, (result) => {
+            expect(removeStub.calledOnce).to.eql(true, 'Expected "removeStub" to be called once');
+            expect(result.step).to.eql(constants.mongo.steps.subgoalDelete, `Expected '${result.step}' to be '${constants.mongo.steps.subgoalDelete}'`);
+            expect(result.message).to.eql('Error deleting subgoal', `Expected '${result.message}' to be 'Error deleting subgoal'`);
             expect(result.error).to.eql('err', `Expected '${result.message}' to be 'err'`);
             done();
         });
